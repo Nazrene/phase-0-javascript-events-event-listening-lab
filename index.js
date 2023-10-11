03290a77f3aa4d44ac9dd07142f6974888fda488
Naz <mercy.kamau@student.moringaschool.com>
2023-10-11 13:40:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+    input.addEventListener('blur',function (){
+        alert('section blurred');
+    });
 }
